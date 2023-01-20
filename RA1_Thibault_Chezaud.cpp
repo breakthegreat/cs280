234e3882c900b8df7a1cf4c4617738af7575fa06
breakthegreat <bloodmodii@gmail.com>
2023-01-19 20:08:08 -0500

    initial commit

---
+++ RA1_Thibault_Chezaud.cpp
@@ -0,0 +1,20 @@
+//====================================================
+// Name : RA1_Thibault_Chezaud
+// Description : Welcome message
+//====================================================
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string first;
+    string last;
+    cout << "Welcome to CS 280 in Spring 2023 Semester" << endl;
+    cout << "Please enter your first name" << endl;
+    cin >> first;
+    cout << "Please enter your last name" << endl;
+    cin >> last;
+    cout << "Welcome " << first << " " << last << " to CS 280" << endl;
+}
